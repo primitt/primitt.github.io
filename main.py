1ee7f0ed7f67bc4dd31e9259b9b4e225f67544f6
primboi <81822069+primitt@users.noreply.github.com>
2024-03-27 15:48:51 -0700

    x

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
-maintainance = False
+maintainance = True
 
 @app.route("/")
 def index():
@@ -17,4 +17,4 @@ def scripts(name):
     return send_from_directory('scripts', name)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003, debug=True)
+    app.run(host="0.0.0.0", port=5003)
