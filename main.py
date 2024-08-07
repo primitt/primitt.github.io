8d8a924aeb2317641632e65ff36c35e739a018a5
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2024-08-06 21:44:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 from flask import Flask, render_template, send_from_directory
-from models.model import *
 app = Flask(__name__)
-maintainance = True
 
 @app.route("/")
 def index():
-    if maintainance:
-        return render_template('construction.html')
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
     
 
-# Load API's
 @app.route('/images/<name>')
 def images(name):
     return send_from_directory('templates/images', name)
 @app.route('/scripts/<name>')
 def scripts(name):
     return send_from_directory('scripts', name)
-
-if __name__ == "__main__":
-    app.run(port=5003)
