55453d120be58326e93b6ac3e20240768dc32f77
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2024-10-15 21:10:19 -0700

    deploy

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('indexnew.html')
     
 
 @app.route('/images/<name>')
@@ -12,3 +12,6 @@ def images(name):
 @app.route('/scripts/<name>')
 def scripts(name):
     return send_from_directory('scripts', name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
